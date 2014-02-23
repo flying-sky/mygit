baf090d7eedcdce2972ff0bc263df89614b05ec6
xiaohui <flying-sky>
2014-02-23 19:23:21 +0800

    add property in personAction.java

--- git/src/com/rihui/aciton/PersonAction.java
+++ git/src/com/rihui/aciton/PersonAction.java
@@ -2,8 +2,24 @@ package com.rihui.aciton;
 
 public class PersonAction {
 	
+	private String pname;
+	private Double price;
 	private Integer age;
 	
+	
+	
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
 	public Integer getAge() {
 		return age;
 	}
