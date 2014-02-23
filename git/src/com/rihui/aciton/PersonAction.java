f62fa7fe4f99a9c1f22f253def0c03cf1b8d2bc5
xiaohui <flying-sky>
2014-02-23 19:10:45 +0800

    fix bug all

--- git/src/com/rihui/aciton/PersonAction.java
+++ git/src/com/rihui/aciton/PersonAction.java
@@ -2,15 +2,8 @@ package com.rihui.aciton;
 
 public class PersonAction {
 	
-	private String pname;
 	private Integer age;
 	
-	public String getPname() {
-		return pname;
-	}
-	public void setPname(String pname) {
-		this.pname = pname;
-	}
 	public Integer getAge() {
 		return age;
 	}
